20a003a0148548ad818570763d01359f0ba8bb9c
chenjian.bzh <chenjian.bzh@bytedance.com>
2023-01-13 19:24:09 +0800

    feat: use webrtc-provider

--- packages/client/monaco.js
+++ packages/client/monaco.js
@@ -4,37 +4,82 @@ import * as Y from 'yjs'
 import { WebsocketProvider } from 'y-websocket'
 import { MonacoBinding } from 'y-monaco'
 import * as monaco from 'monaco-editor'
+import do_username from 'do_username';
+import { WebrtcProvider } from 'y-webrtc'
 
 // @ts-ignore
 window.MonacoEnvironment = {
     getWorkerUrl: function (moduleId, label) {
         if (label === 'json') {
-            return '/monaco/dist/json.worker.bundle.js'
+            return '/dist/json.worker.bundle.js'
         }
         if (label === 'css') {
-            return '/monaco/dist/css.worker.bundle.js'
+            return '/dist/css.worker.bundle.js'
         }
         if (label === 'html') {
-            return '/monaco/dist/html.worker.bundle.js'
+            return '/dist/html.worker.bundle.js'
         }
         if (label === 'typescript' || label === 'javascript') {
-            return '/monaco/dist/ts.worker.bundle.js'
+            return '/dist/ts.worker.bundle.js'
         }
-        return '/monaco/dist/editor.worker.bundle.js'
+        return '/dist/editor.worker.bundle.js'
     }
 }
 
+const usercolors = [
+    '#30bced',
+    '#6eeb83',
+    '#ffbc42',
+    '#ecd444',
+    '#ee6352',
+    '#9ac2c9',
+    '#8acb88',
+    '#1be7ff'
+]
+
 window.addEventListener('load', () => {
     const ydoc = new Y.Doc()
-    const provider = new WebsocketProvider('ws://localhost:8888', 'monaco-demo', ydoc)
+    // const provider = new WebsocketProvider('ws://localhost:8888', 'monaco-demo3', ydoc)
+    // const provider = new WebsocketProvider('wss://demos.yjs.dev', 'monaco-demo3', ydoc)
+    const provider = new WebrtcProvider('awesome drawing', ydoc, { password: 'test' })
+
     const ytext = ydoc.getText('monaco')
 
     const editor = monaco.editor.create(/** @type {HTMLElement} */(document.getElementById('monaco-editor')), {
         value: '',
         language: 'javascript',
         theme: 'vs-dark'
-    })
+    });
+
+    const user = {
+        // clientId: ydoc.clientID,
+        name: do_username.generate(8),
+        color: usercolors[Math.floor(Math.random() * usercolors.length)]
+    }
+
+    provider.awareness.setLocalStateField('user', user);
+
+    provider.awareness.on('change', changes => {
+        provider.awareness.getStates().forEach((state, clientID) => {
+            console.log('oncahgen state: ', state);
+
+            if (clientID === awareness.clientID) {
+                return;
+            }
+
+            const pos = state.pos
+
+            console.log('pos: ', pos)
+            if (!pos) {
+                return
+            }
+
+        })
+    });
+
     const monacoBinding = new MonacoBinding(ytext, /** @type {monaco.editor.ITextModel} */(editor.getModel()), new Set([editor]), provider.awareness)
+
+
 
     const connectBtn = /** @type {HTMLElement} */ (document.getElementById('y-connect-btn'))
     connectBtn.addEventListener('click', () => {
