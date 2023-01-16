0c783bf9ed5c52399aacff673fbaf04fb8e07b27
chenjian.bzh <chenjian.bzh@bytedance.com>
2023-01-16 23:48:38 +0800

    feat: add username style

--- packages/client/monaco.js
+++ packages/client/monaco.js
@@ -1,13 +1,11 @@
 /* eslint-env browser */
-
 import * as Y from 'yjs'
-import { WebsocketProvider } from 'y-websocket'
+// import { WebsocketProvider } from 'y-websocket'
 import { MonacoBinding } from 'y-monaco'
 import * as monaco from 'monaco-editor'
 import do_username from 'do_username';
 import { WebrtcProvider } from 'y-webrtc'
 
-// @ts-ignore
 window.MonacoEnvironment = {
     getWorkerUrl: function (moduleId, label) {
         if (label === 'json') {
@@ -37,10 +35,23 @@ const usercolors = [
     '#1be7ff'
 ]
 
+function addNewStyle(newStyle) {
+    var styleElement = document.getElementById('styles_js');
+
+    if (!styleElement) {
+        styleElement = document.createElement('style');
+        styleElement.type = 'text/css';
+        styleElement.id = 'styles_js';
+        document.getElementsByTagName('head')[0].appendChild(styleElement);
+    }
+
+    styleElement.appendChild(document.createTextNode(newStyle));
+}
+
+
 window.addEventListener('load', () => {
     const ydoc = new Y.Doc()
     // const provider = new WebsocketProvider('ws://localhost:8888', 'monaco-demo3', ydoc)
-    // const provider = new WebsocketProvider('wss://demos.yjs.dev', 'monaco-demo3', ydoc)
     const provider = new WebrtcProvider('awesome drawing', ydoc, { password: 'test' })
 
     const ytext = ydoc.getText('monaco')
@@ -52,10 +63,11 @@ window.addEventListener('load', () => {
     });
 
     const user = {
-        // clientId: ydoc.clientID,
         name: do_username.generate(8),
         color: usercolors[Math.floor(Math.random() * usercolors.length)]
     }
+
+    const loadMap = {};
 
     provider.awareness.setLocalStateField('user', user);
 
@@ -63,17 +75,47 @@ window.addEventListener('load', () => {
         provider.awareness.getStates().forEach((state, clientID) => {
             console.log('oncahgen state: ', state);
 
-            if (clientID === awareness.clientID) {
+            if (clientID === provider.awareness.clientID) {
                 return;
             }
 
-            const pos = state.pos
+            const cs1 = `yRemoteSelection-${clientID}`;
+            const cs2 = `yRemoteSelectionHead-${clientID}`
 
-            console.log('pos: ', pos)
-            if (!pos) {
-                return
+            if (!loadMap[clientID]) {
+
+                loadMap[clientID] = true;
+
+                addNewStyle(
+                    `
+                       .${cs2} {
+                            border-left: ${state.user.color} solid 2px;
+                            border-top: ${state.user.color} solid 2px;
+                            border-bottom: ${state.user.color} solid 2px;
+                       } 
+    
+                       .${cs2}::after {
+                            position: absolute;
+                            content: '${state.user.name}';
+                            background-color: ${state.user.color};
+                            border: 0px;
+                            top: -17px;
+                            left: -1px;
+                            line-height: 1;
+                            padding: 2px 4px;
+                            font-size: 12px;
+                            font-weight: 700;
+                            overflow: hidden;
+                            white-space: nowrap;
+                            user-select: none;
+                            color: #fff;
+                            transform-origin: left bottom;
+                            z-index: 1;
+                            animation: showcursor-small 2.7s cubic-bezier(0,.5,0,1) forwards;
+                        }
+                    `
+                )
             }
-
         })
     });
 
